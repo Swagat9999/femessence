219d12330bac5b2ea8347dc30aa44faa6b21258e
rushi-kesh-19 <nalawaderushikesh221@gmail.com>
2024-08-25 14:09:30 +0530

    BE files

---
+++ be/db/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+});
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imgageLink: [String],
+  published: Boolean
+});  
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imgageLink: [String],
+  published: Boolean,
+  productd: String,
+  rating: Number
+}, { collection: 'products' });
+
+const Admin = mongoose.model('Admin', adminSchema);
+const User = mongoose.model('User', userSchema);
+const Course = mongoose.model('Course', courseSchema);
+const Product = mongoose.model('Product', productSchema);
+
+
+module.exports = { Admin, User, Course, Product };
